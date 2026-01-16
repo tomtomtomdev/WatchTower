f77f0df919cf75cc1aafb927e5cb30d84610a508
tom <tommy.yohanes@icloud.com>
2026-01-16 14:10:29 +0700

    Initial Commit

---
+++ WatchTower/WatchTowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTowerApp.swift
+//  WatchTower
+//
+//  Created by tomtomtom on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchTowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchTowerTests/WatchTowerTests.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTowerTests.swift
+//  WatchTowerTests
+//
+//  Created by tomtomtom on 1/16/26.
+//
+
+import Testing
+@testable import WatchTower
+
+struct WatchTowerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
